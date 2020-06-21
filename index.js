b2c15f6978e365a341a01529eca43ab8d42a2262
difficulttochoose <pmd27041997@gmail.com>
2020-06-21 18:02:21 +0300

    Add functions check

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+console.log('isAdult(20): ', isAdult(20)); // true
+console.log('isAdult(4): ', isAdult(4)); // false
+
+console.log('checkMultiplicity(25, 5): ', checkMultiplicity(25, 5)); // true
+console.log('checkMultiplicity(15, 3): ', checkMultiplicity(15, 3)); // true
+console.log('checkMultiplicity(15, 5): ', checkMultiplicity(15, 5)); // true
+console.log('checkMultiplicity(15, 4): ', checkMultiplicity(15, 4)); // false
+
+console.log('isTriangle(6, 2, 5): ', isTriangle(6, 2, 5)); //true
+console.log('isTriangle(1, 2, 3): ', isTriangle(1, 2, 3)); //false
+
+console.log('areaOfATriangle(4, 5, 6): ', areaOfATriangle(4, 5, 6));
+console.log('areaOfATriangle(35, 92, 59): ', areaOfATriangle(35, 92, 59));
+console.log('areaOfATriangle(1, 2, 3): ', areaOfATriangle(1, 2, 3));
+
+console.log('areaOfARhombus(4, 7): ', areaOfARhombus(4, 7));
+
+console.log('surfaceAreaOfACylinder(7, 19): ', surfaceAreaOfACylinder(7, 19));
+console.log('surfaceAreaOfACylinder(8.56, 3.89): ', surfaceAreaOfACylinder(8.56, 3.89));
+
+console.log('areaOfARectangle(16, 79): ', areaOfARectangle(16, 79));
 
 /**
  * Check user age for majority
